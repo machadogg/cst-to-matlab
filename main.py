c8168ee197984f55ee44cf5acf3c7daf8df5ea8b
GabrielMachado <35139989+machadogg@users.noreply.github.com>
2018-01-08 22:05:40 +0000

    finished_V1

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from functions import *
 import re
 
-rfn = input("Enter the name for the file to be converted: ")
-wfw = input("Enter the output file name: ")
+rfn = input("Enter the name for the file to be converted (without extension): ")
+wfw = input("Enter the output file name (without extension): ")
 
-with open("test4.txt", 'r') as rf:
+rfn = rfn + '.txt'
+wfw = wfw + '.csv'
+
+with open(rfn, 'r') as rf:
 
     text = rf.read()
     liss = re.split("\n", text)
@@ -13,7 +16,7 @@ with open("test4.txt", 'r') as rf:
     printdeb()
     liss = define_Sparam(liss)
 
-    with open("write3.csv", "w") as wf:
+    with open(wfw, "w") as wf:
 
         for item in liss:
             wf.write(item + '\n')
