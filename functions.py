31daec9ffa82a57421a8d8e6b96a9b59775d9894
GabrielMachado <35139989+machadogg@users.noreply.github.com>
2018-01-09 14:25:49 +0000

    improved for parameter sweep

--- functions.py
+++ functions.py
@@ -23,7 +23,7 @@ def del_dashes(lista):
 # adjust the structure to prepare the final CSV file
 
 
-def define_Sparam(lis):
+def define_Sparam(lis, test):
 
     k = 0
 
@@ -42,138 +42,149 @@ def define_Sparam(lis):
     # this 'for' loop detects whether we are reading
     # the results from a floquet port (SZmin(1),Zmax(1)) - converts to s11-te or tm, depending on the index
     # or from a waveguide/discrete port (S1,1) - converts to s11, or s12, or s22 or s21
+
     for i in range(len(lis)):
+        if test == 'N':
 
-        if 'SZmin(1),Zmax(1)' in lis[i]:
-            if i > 0:
-                lis[0] = lis[0] + ',f,s21-te'
-                lis[i] = ''
-            else:
-                lis[i] = 'f,s21-te'
-            k += 1
+            if 'SZmin(1),Zmax(1)' in lis[i]:
+                if i > 0:
+                    lis[0] = lis[0] + ',f,s21-te'
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,s21-te'
+                k += 1
 
-        elif 'SZmax(1),Zmax(1)' in lis[i]:
-            if i > 0:
-                lis[0] = lis[0] + ',f,s11-te'
-                lis[i] = ''
-            else:
-                lis[i] = 'f,s11-te'
-            k += 1
+            elif 'SZmax(1),Zmax(1)' in lis[i]:
+                if i > 0:
+                    lis[0] = lis[0] + ',f,s11-te'
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,s11-te'
+                k += 1
 
-        elif 'SZmax(1),Zmin(1)' in lis[i]:
-            if i > 0:
-                lis[0] = lis[0] + ',f,s12-te'
-                lis[i] = ''
-            else:
-                lis[i] = 'f,s12-te'
-            k += 1
+            elif 'SZmax(1),Zmin(1)' in lis[i]:
+                if i > 0:
+                    lis[0] = lis[0] + ',f,s12-te'
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,s12-te'
+                k += 1
 
-        elif 'SZmin(1),Zmin(1)' in lis[i]:
-            if i > 0:
-                lis[0] = lis[0] + ',f,s22-te'
-                lis[i] = ''
-            else:
-                lis[i] = 'f,s22-te'
-            k += 1
+            elif 'SZmin(1),Zmin(1)' in lis[i]:
+                if i > 0:
+                    lis[0] = lis[0] + ',f,s22-te'
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,s22-te'
+                k += 1
 
-        elif 'SZmin(2),Zmax(2)' in lis[i]:
-            if i > 0:
-                lis[0] = lis[0] + ',f,s21-tm'
-                lis[i] = ''
-            else:
-                lis[i] = 'f,s21-tm'
-            k += 1
+            elif 'SZmin(2),Zmax(2)' in lis[i]:
+                if i > 0:
+                    lis[0] = lis[0] + ',f,s21-tm'
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,s21-tm'
+                k += 1
 
-        elif 'SZmax(2),Zmax(2)' in lis[i]:
-            if i > 0:
-                lis[0] = lis[0] + ',f,s11-tm'
-                lis[i] = ''
-            else:
-                lis[i] = 'f,s11-tm'
-            k += 1
+            elif 'SZmax(2),Zmax(2)' in lis[i]:
+                if i > 0:
+                    lis[0] = lis[0] + ',f,s11-tm'
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,s11-tm'
+                k += 1
 
-        elif 'SZmax(2),Zmin(2)' in lis[i]:
-            if i > 0:
-                lis[0] = lis[0] + ',f,s12-tm'
-                lis[i] = ''
-            else:
-                lis[i] = 'f,s12-tm'
-            k += 1
+            elif 'SZmax(2),Zmin(2)' in lis[i]:
+                if i > 0:
+                    lis[0] = lis[0] + ',f,s12-tm'
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,s12-tm'
+                k += 1
 
-        elif 'SZmin(2),Zmin(2)' in lis[i]:
-            if i > 0:
-                lis[0] = lis[0] + ',f,s22-tm'
-                lis[i] = ''
-            else:
-                lis[i] = 'f,s22-tm'
-            k += 1
+            elif 'SZmin(2),Zmin(2)' in lis[i]:
+                if i > 0:
+                    lis[0] = lis[0] + ',f,s22-tm'
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,s22-tm'
+                k += 1
 
-        elif 'S1,1' in lis[i]:
-            if i > 0:
-                lis[0] = lis[0] + ',f,s11'
-                lis[i] = ''
-            else:
-                lis[i] = 'f,s11'
-            k += 1
-        elif 'S2,1' in lis[i]:
-            if i > 0:
-                lis[0] = lis[0] + ',f,s21'
-                lis[i] = ''
-            else:
-                lis[i] = 'f,s21'
-            k += 1
-        elif 'S1,2' in lis[i]:
-            if i > 0:
-                lis[0] = lis[0] + ',f,s12'
-                lis[i] = ''
-            else:
-                lis[i] = 'f,s12'
-            k += 1
-        elif 'S2,2' in lis[i]:
-            if i > 0:
-                lis[0] = lis[0] + ',f,s22'
-                lis[i] = ''
-            else:
-                lis[i] = 'f,s22'
-            k += 1
-        elif 'Z1,1' in lis[i]:
-            if i > 0:
-                lis[0] = lis[0] + ',f,z11'
-                lis[i] = ''
-            else:
-                lis[i] = 'f,z11'
-            k += 1
-        elif 'Z2,1' in lis[i]:
-            if i > 0:
-                lis[0] = lis[0] + ',f,z21'
-                lis[i] = ''
-            else:
-                lis[i] = 'f,z21'
-            k += 1
-        elif 'Z1,2' in lis[i]:
-            if i > 0:
-                lis[0] = lis[0] + ',f,z12'
-                lis[i] = ''
-            else:
-                lis[i] = 'f,z12'
-            k += 1
-        elif 'Z2,2' in lis[i]:
-            if i > 0:
-                lis[0] = lis[0] + ',f,z22'
-                lis[i] = ''
-            else:
-                lis[i] = 'f,z22'
-            k += 1
-        elif 'F' in lis[i]: #If it doesn't match the patterns, generates generic name from CST
-            ln = lis[i].split()
-            param = ln[3].replace(',', '')
-            if i > 0:
-                lis[0] = lis[0] + ',f,' + param
-                lis[i] = ''
-            else:
-                lis[i] = ',f,' + param
-            k += 1
-
+            elif 'S1,1' in lis[i]:
+                if i > 0:
+                    lis[0] = lis[0] + ',f,s11'
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,s11'
+                k += 1
+            elif 'S2,1' in lis[i]:
+                if i > 0:
+                    lis[0] = lis[0] + ',f,s21'
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,s21'
+                k += 1
+            elif 'S1,2' in lis[i]:
+                if i > 0:
+                    lis[0] = lis[0] + ',f,s12'
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,s12'
+                k += 1
+            elif 'S2,2' in lis[i]:
+                if i > 0:
+                    lis[0] = lis[0] + ',f,s22'
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,s22'
+                k += 1
+            elif 'Z1,1' in lis[i]:
+                if i > 0:
+                    lis[0] = lis[0] + ',f,z11'
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,z11'
+                k += 1
+            elif 'Z2,1' in lis[i]:
+                if i > 0:
+                    lis[0] = lis[0] + ',f,z21'
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,z21'
+                k += 1
+            elif 'Z1,2' in lis[i]:
+                if i > 0:
+                    lis[0] = lis[0] + ',f,z12'
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,z12'
+                k += 1
+            elif 'Z2,2' in lis[i]:
+                if i > 0:
+                    lis[0] = lis[0] + ',f,z22'
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,z22'
+                k += 1
+            elif 'F' in lis[i]:  # If it doesn't match the patterns, generates generic name from CST
+                ln = lis[i].split()
+                param = ln[3].replace(',', '')
+                if i > 0:
+                    lis[0] = lis[0] + ',f,' + param
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,' + param
+                k += 1
+        else:
+            if 'F' in lis[i]:
+                ln = lis[i].split()
+                param = ln[3].replace(',', '')
+                if i > 0:
+                    lis[0] = lis[0] + ',f,' + param
+                    lis[i] = ''
+                else:
+                    lis[i] = 'f,' + param
+                k += 1
         # Eliminate the white spaces and puts a comma for the CSV
         if i > 0:
             lis[i] = ','.join(lis[i].split())
--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import os
 
 rfn = input("Enter the name for the file to be converted (without extension): ")
 wfw = input("Enter the output file name (without extension): ")
+par = ''
+
+while True:
+    par = input("Is the plot a parameter sweep? (Y/N): ")
+    par = par.upper()
+    if par == 'N' or par == 'Y':
+        break
+    print(par)
 
 rfn = rfn + '.txt'
 wfw = wfw + '.csv'
@@ -16,7 +24,7 @@ try:
 
         liss = del_dashes(liss)
         # printdeb()
-        liss = define_Sparam(liss)
+        liss = define_Sparam(liss, par)
 
         with open(wfw, "w") as wf:
 
